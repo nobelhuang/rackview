42e6d5e59be66b315eefa523f1c698d8fe1eefc9
Nobel Huang <nobel1984@gmail.com>
2017-09-29 00:32:42 -0700

    set up the env based on a typescript-webpack template

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default {
+  name: "app",
+} as Vue.ComponentOptions<Vue>;
+</script>
+
+<style>
+</style>
+
